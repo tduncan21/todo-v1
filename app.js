589acaf6b08421fde7192c23f8ca87ad21bc9307
Tim <tduncan94@gmail.com>
2021-03-01 09:05:54 -0600

    Add database to save lists

--- app.js
+++ app.js
@@ -1,40 +1,132 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
+const _ = require('lodash');
+// const date = require(__dirname + "/date.js");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.use(express.static("components"));
+// app.use(express.static("components"));
 app.set('view engine', 'ejs');
 
-let items = [];
-let workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todo list!"
+});
+const item2 = new Item({
+    name: "Hit the '+' button to add a new item"
+});
+const item3 = new Item({
+    name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
 
 
 app.get('/', (req, res) => {
-    let day = date.getDate();
-    res.render('list', {listTitle: day, itemList: items});
+    // let day = date.getDate();
+    Item.find({}, (err, results) => {
+        if(err) {
+            console.log("First: " + err);
+        } else {
+            if(results.length === 0){
+                Item.insertMany(defaultItems, (err) => {
+                    if(err){
+                        console.log("Second: " + err);
+                    } else {
+                        console.log("Default items added");
+                    }
+                    res.redirect("/");
+                });
+            } else {
+                res.render('list', {listTitle: "Today", itemList: results});
+            }
+        }
+    });    
 });
 
-app.get('/work', (req, res)=>{
-    res.render("list", {listTitle: "Work List", itemList: workItems});
-})
+app.get('/:listName', (req, res)=>{
+    const customListName = _.capitalize(req.params.listName)
+    
+    List.findOne({name: customListName}, (err, results) => {
+        if(!err) {
+            if(!results) {
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect("/" + customListName);
+            } else {
+                res.render("list", {listTitle: results.name, itemList: results.items});
+            }
+        } else {
+            console.log(err);            
+        }
+    });
+});
 
 app.post('/', (req, res) => {
-    item = req.body.newItem;
-    if(req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect("/work")
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
+    const item = new Item({
+        name: itemName
+    });
+
+    if(listName === "Today") {
+        item.save();
+        res.redirect("/");
     } else {
-        items.push(item);
-        res.redirect('/');
-    }    
+        List.findOne({name: listName}, (err, results) => {
+            results.items.push(item);
+            results.save();
+            res.redirect("/" + listName);
+        })
+    }
 });
 
-app.get('/remove', (req, res) => {
-    items.pop();
-    res.redirect('/');
+app.post('/remove', (req, res) => {
+    const itemId = req.body.checkbox;
+    const listName = req.body.listName;
+
+    if(listName === "Today"){
+        // Default list
+        Item.findByIdAndRemove(itemId, (err) => {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("Item deleted");
+            }
+        });
+        res.redirect("/");
+    } else {
+        List.findOneAndUpdate(
+            {name: listName},
+            {$pull: {items: {_id: itemId}}},
+            (err, results)=>{
+                if(err){
+                    console.log(err);
+                } else {
+                    res.redirect("/" + listName);
+                }
+            }
+        )
+    }    
 });
 
 app.listen(process.env.PORT || 3000, () => {
